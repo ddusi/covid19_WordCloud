1134facb51143fee3d3d045b8854d95fce49d54b
ddusi <ddusi@kakao.com>
2020-10-23 21:00:10 +0900

    feature(form): test form QnA page

--- Covid_web/urls.py
+++ Covid_web/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     path('covid-info', views.covid_info, name='covid-info'),
     path('qna', views.qna, name='qna'),
     path('new-question',views.new_question, name='new_question'),
-    path('create',views.create, name='create'),
+    # path('create',views.create, name='create'),
     path('precautions', views.precautions, name='precautions'),
-    path('<int:question_id>/', views.question, name='question'),
+    path('<int:question_id>/', views.question_read, name='question'),
     path('<int:question_id>', views.answer, name='answer'),
+    path('test', views.create_question, name='test'),
+    # path('created/question', views.create_question, name='create_question'),
 ]
--- Covid_web/views/views.py
+++ Covid_web/views/views.py
@@ -6,6 +6,7 @@ from ..helper.get_info import basic, precaution, Covid_confirmed, Make_Cloud
 from ..helper.make_cloud_helper import make_cloud_helper
 import threading
 import pandas as pd
+from ..forms import QuestionForm
 
 flag = True
 article = {}
@@ -33,17 +34,17 @@ def make():
 	timer.start()
 
 
-make()
+# make()
 
 
 def home(request):
-	return render(request, 'Covid_web/home.html')
+	return render(request, 'covid_web/home.html')
 
 
 def news(request):
 	global article
 	context = article
-	return render(request, 'Covid_web/news.html', context)
+	return render(request, 'covid_web/news.html', context)
 
 
 def covid_info(request):
@@ -51,30 +52,18 @@ def covid_info(request):
 	context = {
 		'cont': cont[1:-21]
 	}
-	return render(request, 'Covid_web/covid_info.html', context)
+	return render(request, 'covid_web/covid_info.html', context)
 
 
 def qna(request):
 	questions = Question.objects
-	return render(request, 'Covid_web/qna.html', {'object': Question, 'questions': questions})
+	return render(request, 'covid_web/qna.html', {'object': Question, 'questions': questions})
 
 
-def question(request, question_id):
+def question_read(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	answers = question.answers.all()
-	return render(request, 'Covid_web/question.html', {'object': Question, 'question': question, 'answers': answers})
-
-
-def new_question(request):
-	return render(request, 'Covid_web/new_question.html')
-
-
-def create(request):
-	if (request.method == 'POST'):
-		post = Question()
-		post.question_text = request.POST['question_text']
-		post.save()
-	return redirect('covid:qna')
+	return render(request, 'covid_web/question.html', {'object': Question, 'question': question, 'answers': answers})
 
 
 def answer(request, question_id):
@@ -86,6 +75,10 @@ def answer(request, question_id):
 	return HttpResponseRedirect(reverse_lazy('covid:question', args=[question_id]))
 
 
+def new_question(request):
+	return render(request, 'covid_web/new_question.html')
+
+
 def precautions(request):
 	global pre, Korea, World
 	context = {
@@ -93,4 +86,27 @@ def precautions(request):
 		'Korea': Korea,
 		'World': World,
 	}
-	return render(request, 'Covid_web/precautions.html', context)
+	return render(request, 'covid_web/precautions.html', context)
+
+
+def test(request):
+	return render(request, 'covid_web/test.html')
+
+
+def create_question(request):
+	questions = Question.objects
+	if request.method == 'POST':
+		form = QuestionForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/covid-web/test', {'questions': questions})
+	else:
+		form = QuestionForm()
+	return render(request, 'covid_web/test.html', {'form': form,'questions': questions})
+
+# def create_question(request):
+# 	if request.method == 'POST':
+# 		form = QuestionForm(request.POST)
+# 		if form.is_valid():
+# 			form.save()
+# 		return redirect('/test')
