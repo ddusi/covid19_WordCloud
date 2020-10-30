39889a2af715fda3908cfbfb530ce24f611ddfcb
ddusi <ddusi@kakao.com>
2020-10-30 19:54:32 +0900

    update(settings): Templates dirs add

--- Covid/settings.py
+++ Covid/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Covid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['[Covid]/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
