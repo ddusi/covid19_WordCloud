2d0ab35c1d804e589d02430f895007dcc476467d
ddusi <ddusi@kakao.com>
2020-11-02 19:53:55 +0900

    feature(html): append create crawling world data function

--- covid_web/apps.py
+++ covid_web/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 from multiprocessing import Process
 import sys
 from .views.views import make_data
+from .helper.covid19_world_confirmation_helper import covid_confirmation
 import time
 
 
@@ -13,4 +14,6 @@ class CovidWebConfig(AppConfig):
 			return True
 
 		process_one = Process(target=make_data)
+		process_two = Process(target=covid_confirmation)
 		process_one.start()
+		process_two.start()
