6283e04d28d594a393f81af612663729ae11c5de
ddusi <ddusi@kakao.com>
2020-11-03 03:19:56 +0900

    feture(helper): dataFrame import success

--- covid_web/views/status_view.py
+++ covid_web/views/status_view.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 import pandas as pd
-from ..helper.covid19_world_confirmation_helper import remove_comma, covid_confirmation
-# from ..helper.save_dataframe_helper import return_data_frame
+from covid_web.helper.covid19_world_confirmation_helper import remove_comma, covid_confirmation
+from covid_web.helper.save_dataframe_helper import return_data_frame
+import plotly.express as px
+from plotly.offline import plot
+# import plotly.offline.offline as plot
 
 def status(request):
-	# df = return_data_frame('world_confirmation')
+	df = return_data_frame('world_confirmation')
 
 	df['Total Rank'] = df['Total'].apply(remove_comma).astype(int)
 	df['Total Rank'] = df['Total Rank'] / df['Total Rank'].sum() * 100
-
-	import plotly.express as px
-	import plotly.offline as plot
 
 	fig = px.pie(df, values='Total Rank', names='Area', hover_name='Total')
 	fig.update_traces(hole=.4, hoverinfo="label+percent+name")
@@ -18,8 +18,8 @@ def status(request):
 		title_text="World Covid19 confirmation",
 		# Add annotations in the center of the donut pies.
 		annotations=[dict(text='Covid19', x=0.5, y=0.5, font_size=20, showarrow=False)])
-	fig.show()
+	# confirmation_graph = fig.to_html(full_html=False)
+	confirmation_graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+	context = {'confirmation_graph': confirmation_graph}
 
-	confirmation_graph = plot(fig, include_plotlyjs=False, output_type='div')
-
-	return render(request, 'covid_web/status.html', {'df' : df, 'html_data' : confirmation_graph})
+	return render(request, 'covid_web/status.html', context)
