2fd4f826461b6cc805f7dc17d77cb150af2528dc
ddusi <ddusi@kakao.com>
2020-11-03 18:03:19 +0900

    feature(models): append Trends column

--- covid_web/models.py
+++ covid_web/models.py
@@ -27,6 +27,7 @@ class WorldConfirmation(models.Model):
     Cases = models.TextField(null=True)
     Recovered = models.TextField(null=True)
     Deaths = models.TextField(null=True)
+    Trends = models.TextField(null=True)
     created_at = models.DateTimeField(null=True)
 
     class Meta:
