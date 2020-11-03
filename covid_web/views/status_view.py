03ab25f95bde48b59107b57748ec245bf097958e
ddusi <ddusi@kakao.com>
2020-11-03 23:39:23 +0900

    update(html): status_views add to graph

--- covid_web/views/status_view.py
+++ covid_web/views/status_view.py
@@ -3,6 +3,7 @@ import pandas as pd
 from covid_web.helper.covid19_world_confirmation_helper import remove_comma
 from covid_web.helper.save_dataframe_helper import return_data_frame
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 # from plotly.offline import plot
@@ -23,6 +24,24 @@ def status(request):
 		annotations=[dict(text='Covid19', x=0.5, y=0.5, font_size=20, showarrow=False)])
 	# confirmation_graph = fig.to_html(full_html=False)
 	confirmation_graph = fig.to_html(full_html=False, default_height=500, default_width=700)
-	context = {'world_data': df, 'confirmation_graph': confirmation_graph}
+
+	## Cumulative COVID-19 deaths on Jan 11, and first day of following months
+	data = {'month': ['jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+	        'case': [1, 259, 2977, 40598, 224172, 371166, 508055, 675060, 848445, 1010639]}
+	df = pd.DataFrame(data)
+
+	fig = go.Figure(go.Scatter(
+		x=df['month'],
+		y=df['case'],
+	))
+
+	fig.update_layout(
+		title='Cumulative COVID-19 deaths on Jan 11, and first day of following months',
+	)
+	cumulative_deaths = fig.to_html(full_html=False, default_height=500, default_width=700)
+
+	context = {'world_data': df,
+	           'confirmation_graph': confirmation_graph,
+	           'cumulative_deaths': cumulative_deaths}
 
 	return render(request, 'covid_web/status.html', context)
