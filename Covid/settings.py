88d6cf2a364264ef8aa5df30d84e7430c226efa5
ddusi <ddusi@kakao.com>
2020-11-03 22:58:39 +0900

    update(requirements): append libs - plotly

--- Covid/settings.py
+++ Covid/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from os.path import join
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
